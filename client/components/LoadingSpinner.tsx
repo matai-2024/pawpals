ce520dcf3652f0f0766db38538991b85446050f8
Anahera Foley-Paama <a.foleypaama@gmail.com>
2024-09-03 09:30:05 +1200

    Create loading spinner

---
+++ client/components/LoadingSpinner.tsx
@@ -0,0 +1,9 @@
+export default function LoadingSpinner() {
+  return (
+    <>
+      <div className="h-screen flex content-center justify-center">
+        <div className="loader"></div>
+      </div>
+    </>
+  )
+}
