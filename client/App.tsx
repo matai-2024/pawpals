79f59a1ecea4c63d08259e794f6ec82aab4e9fad
Anahera Foley-Paama <a.foleypaama@gmail.com>
2024-09-07 20:27:16 +1200

    Create background component

--- client/App.tsx
+++ client/App.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router-dom'
 import Nav from './components/Nav.tsx'
+import Background from './components/utils/Background.tsx'
 
 function App() {
   return (
     <div className="container">
       <Nav />
-      <Outlet />
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <Background />
+        <Outlet />
+      </div>
     </div>
   )
 }
