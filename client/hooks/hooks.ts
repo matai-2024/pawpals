b307a549f292ee9838f130180d67b515806358e3
Anahera Foley-Paama <a.foleypaama@gmail.com>
2024-09-11 21:13:30 +1200

    Tidy up client hooks

--- client/hooks/hooks.ts
+++ client/hooks/hooks.ts
@@ -1,2 +1,9 @@
-export { default as useCreatePet } from './use-create-pet.ts'
+export { default as usePets } from './use-pets.ts'
+export { default as usePetById } from './use-pet-by-id.ts'
+export { default as usePetList } from './use-pet-list.ts'
+export { default as useCreatePet } from './use-pet-create.ts'
 export { default as useMultistepForm } from './use-multistep-form.ts'
+
+// TODO:
+// delete pets
+// update the edit pets api client function and hook
---
+++ client/hooks/use-pet-by-id.ts
@@ -0,0 +1,10 @@
+import { useQuery } from "@tanstack/react-query"
+import { fetchPetById } from "../apis/apiClientPets.ts"
+
+export default function usePetById(id: number) {
+  const query = useQuery({
+    queryKey: ['pets', id],
+    queryFn: () => fetchPetById(id),
+  })
+  return { ...query }
+}
