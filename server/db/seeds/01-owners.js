a1048ccb0f8ccb3eac42cfbc5d58170987ae496b
Anahera Foley-Paama <a.foleypaama@gmail.com>
2024-09-11 20:31:22 +1200

    Update seed data

--- server/db/seeds/01-owners.js
+++ server/db/seeds/01-owners.js
@@ -19,15 +19,75 @@ export const addOwners = [
   },
   {
     id: 4,
+    first_name: 'Dean',
+    last_name: 'Tunbridge',
+    email: 'dean@email.com',
+  },
+  {
+    id: 5,
+    first_name: 'Sam',
+    last_name: 'Pederson',
+    email: 'sam@email.com',
+  },
+  {
+    id: 6,
     first_name: 'Logan',
     last_name: 'Bennett',
     email: 'logan@email.com',
   },
   {
-    id: 5,
+    id: 7,
     first_name: 'Alicia',
     last_name: 'Tavita',
     email: 'alicia@email.com',
+  },
+  {
+    id: 8,
+    first_name: 'Elias',
+    last_name: 'Kaleta',
+    email: 'elias@email.com',
+  },
+  {
+    id: 9,
+    first_name: 'Ryan',
+    last_name: 'Zhao',
+    email: 'ryan@email.com',
+  },
+  {
+    id: 10,
+    first_name: 'Emma',
+    last_name: 'Johnson',
+    email: 'emma.johnson@email.com',
+  },
+  {
+    id: 11,
+    first_name: 'Liam',
+    last_name: 'Smith',
+    email: 'liam.smith@email.com',
+  },
+  {
+    id: 12,
+    first_name: 'Olivia',
+    last_name: 'Williams',
+    email: 'olivia.williams@email.com',
+  },
+  {
+    id: 13,
+    first_name: 'Noah',
+    last_name: 'Brown',
+    email: 'noah.brown@email.com',
+  },
+  {
+    id: 14,
+    first_name: 'Ava',
+    last_name: 'Davis',
+    email: 'ava.davis@email.com',
+  },
+  {
+    id: 15,
+    first_name: 'Ethan',
+    last_name: 'Miller',
+    email: 'ethan.miller@email.com',
   },
 ]
 
