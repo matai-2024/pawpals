67adebd200a294ff753369cb7aa04d208ba02bc2
Dean <171200457+dean-tunbridge@users.noreply.github.com>
2024-09-13 10:24:56 +1200

    create temp event data, begin EventList component

---
+++ client/components/utils/tempEvents.tsx
@@ -0,0 +1,47 @@
+import { EventId } from '../../../models/events'
+
+export const Events: EventId[] = [
+  {
+    id: 1,
+    title: 'Grandpas birthday',
+    date: '16-06-2024',
+    location: 'Mission Bay',
+    description: 'Grandpa is having a birthday bbq',
+    creatorId: 1,
+  },
+  {
+    id: 2,
+    title: 'New year party',
+    date: '01-01-2025',
+    location: 'Grey Lynn',
+    description: 'Happy new year',
+    creatorId: 5,
+  },
+  {
+    id: 3,
+    title: 'Obi is having a birthday party',
+    date: '20-10-2024',
+    location: 'Waiheke',
+    description: 'Obis birthday party',
+    creatorId: 1,
+  },
+  {
+    id: 4,
+    title: 'Mutual friends coffee meetup',
+    date: '28-09-2024',
+    location: 'Newmarket',
+    description: 'Mutual friends cafe are having a bring your pet event',
+    creatorId: 3,
+  },
+  {
+    id: 5,
+    title: 'My Iguana is missing',
+    date: '26-09-2024',
+    location: 'North shore',
+    description:
+      'My pet iguana has escaped. Pleasse come to the park in birkenhead and help me look for him!',
+    creatorId: 1,
+  },
+]
+
+export default Events
---
+++ client/pages/EventList.tsx
@@ -0,0 +1,16 @@
+import { Events } from '../components/utils/tempEvents'
+
+export function EventList() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+        Event List
+      </h1>
+      <p className="mt-6 text-lg leading-8 text-gray-600">
+        A list of all upcoming events in your area...
+      </p>
+    </>
+  )
+}
+
+export default EventList
