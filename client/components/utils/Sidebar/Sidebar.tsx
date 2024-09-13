43533c81f0077da863f6d2dec7cd57617c89aa47
Sam Pedersen <sjcfpedersen@gmail.com>
2024-09-13 17:31:36 +1200

    add links

--- client/components/utils/Sidebar/Sidebar.tsx
+++ client/components/utils/Sidebar/Sidebar.tsx
@@ -13,28 +13,28 @@ const Sidebar = () => {
       </div>
       <div className="flex flex-col space-y-6 mt-10 pl-6">
         <a
-          href="linkhere"
+          href="/"
           className="flex items-center space-x-3 text-gray-700 hover:text-black"
         >
           <i className="fas fa-home"></i>
           <span>Home</span>
         </a>
         <a
-          href="linkhere"
+          href="/profiles"
           className="flex items-center space-x-3 text-gray-700 hover:text-black"
         >
           <i className="fas fa-paw"></i>
           <span>My Pets</span>
         </a>
         <a
-          href="linkhere"
+          href="/events"
           className="flex items-center space-x-3 text-gray-700 hover:text-black"
         >
           <i className="fas fa-calendar-alt"></i>
           <span>My Schedule</span>
         </a>
         <a
-          href="linkhere"
+          href="/events"
           className="flex items-center space-x-3 text-gray-700 hover:text-black"
         >
           <i className="fas fa-calendar"></i>
