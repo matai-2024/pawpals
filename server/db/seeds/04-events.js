9bf37dedc17b717636dce6767ba867f6ba81f7df
Anahera Foley-Paama <a.foleypaama@gmail.com>
2024-09-13 19:06:07 +1200

    Update events column names

--- server/db/seeds/04-events.js
+++ server/db/seeds/04-events.js
@@ -7,9 +7,9 @@ export const addEvents = [
     location: 'Lloyd Elsmore Park, Sir Lloyd Drive, Auckland',
     description:
       '🍔🌮 Experience the Best of Street Food at Howick’s Ultimate Food Truck Night! 🚚🎉 Join us in Howick for an exciting Food Truck Night that promises a feast for all your senses! Get ready to dive into a world of flavors as the best food trucks in town come together to serve up mouthwatering dishes that will leave you craving more.',
-    eventImage:
+    event_image:
       'https://eastaucklandtourism.co.nz/wp-content/uploads/2024/08/Food-Truck.jpg',
-    eventWebsite:
+    event_website:
       'https://www.eventfinda.co.nz/2024/food-trucks-in-howick/auckland/pakuranga',
     audience: 'Anyone!',
     creator_id: 1,
@@ -22,8 +22,8 @@ export const addEvents = [
     location: 'The Hoppy Hound Brewery, 123 Bark Avenue, Dogtown',
     description:
       '🍺🐶 Grab your furry friend and join us for Paws & Pints, a brewery tour where dogs are more than welcome! Enjoy craft beers, dog-friendly treats, and live music while mingling with fellow pet lovers.',
-    eventImage: 'https://example.com/images/paws_and_pints.jpg',
-    eventWebsite: 'https://www.example.com/paws-and-pints',
+    event_image: 'https://example.com/images/paws_and_pints.jpg',
+    event_website: 'https://www.example.com/paws-and-pints',
     audience: 'Dog Owners',
     creator_id: 2,
   },
@@ -35,8 +35,8 @@ export const addEvents = [
     location: 'Green Meadows Riding Club, 456 Gallop Lane, Equestriatown',
     description:
       '🏇✨ Join us for an elegant evening at the Equestrian Gala! Enjoy dressage performances, horse demonstrations, and a silent auction to benefit equine charities. Perfect for horse enthusiasts and their families.',
-    eventImage: 'https://example.com/images/equestrian_gala.jpg',
-    eventWebsite: 'https://www.example.com/equestrian-gala',
+    event_image: 'https://example.com/images/equestrian_gala.jpg',
+    event_website: 'https://www.example.com/equestrian-gala',
     audience: 'Horse Lovers',
     creator_id: 3,
   },
@@ -48,8 +48,8 @@ export const addEvents = [
     location: 'Purrfect Yoga Studio, 789 Meow Road, Catville',
     description:
       '🧘‍♂️🐱 Stretch, relax, and play with adoptable cats at our Cat Yoga session! Enjoy guided yoga with feline friends, cat-themed crafts, and a meet-and-greet with local shelters.',
-    eventImage: 'https://example.com/images/cat_yoga.jpg',
-    eventWebsite: 'https://www.example.com/feline-fun-fest',
+    event_image: 'https://example.com/images/cat_yoga.jpg',
+    event_website: 'https://www.example.com/feline-fun-fest',
     audience: 'Cat Lovers',
     creator_id: 4,
   },
@@ -61,8 +61,8 @@ export const addEvents = [
     location: 'Central Park, 321 Parade Street, Pet City',
     description:
       '🎃🐾 Dress up your pets and join us for a spooktacular Pet Costume Parade! Prizes for the best costumes, pet-friendly treats, and plenty of fun for pets and their humans. Don’t miss this Halloween-themed event!',
-    eventImage: 'https://example.com/images/pet_costume_parade.jpg',
-    eventWebsite: 'https://www.example.com/pet-costume-parade',
+    event_image: 'https://example.com/images/pet_costume_parade.jpg',
+    event_website: 'https://www.example.com/pet-costume-parade',
     audience: 'Pet Owners',
     creator_id: 5,
   },
@@ -74,8 +74,8 @@ export const addEvents = [
     location: 'Sunnyvale Park, 567 Wing Street, Birdtown',
     description:
       '🦜🌳 Enjoy a day of birdwatching with fellow avian enthusiasts! Bring your binoculars, a picnic lunch, and learn about local bird species while your feathered friends enjoy a treat-filled picnic.',
-    eventImage: 'https://example.com/images/birdwatching_picnic.jpg',
-    eventWebsite: 'https://www.example.com/birdwatching-picnic',
+    event_image: 'https://example.com/images/birdwatching_picnic.jpg',
+    event_website: 'https://www.example.com/birdwatching-picnic',
     audience: 'Bird Lovers',
     creator_id: 6,
   },
@@ -87,8 +87,8 @@ export const addEvents = [
     location: 'Agility Arena, 890 Jump Road, Dogtown',
     description:
       '🏅🐕 Watch talented dogs navigate challenging agility courses in this exciting competition! Perfect for dogs of all skill levels, with awards for speed, precision, and best overall performance.',
-    eventImage: 'https://example.com/images/dog_agility.jpg',
-    eventWebsite: 'https://www.example.com/dog-agility-competition',
+    event_image: 'https://example.com/images/dog_agility.jpg',
+    event_website: 'https://www.example.com/dog-agility-competition',
     audience: 'Dog Owners and Enthusiasts',
     creator_id: 7,
   },
@@ -100,8 +100,8 @@ export const addEvents = [
     location: 'Tiny Pets Park, 234 Squeak Lane, Hamsterville',
     description:
       '🚴🐹 Cheer on hamsters as they race in their very own wheel derby! With fun races, obstacle courses, and a chance to meet local hamster enthusiasts, this event is a must for small pet lovers.',
-    eventImage: 'https://example.com/images/hamster_derby.jpg',
-    eventWebsite: 'https://www.example.com/hamster-wheel-derby',
+    event_image: 'https://example.com/images/hamster_derby.jpg',
+    event_website: 'https://www.example.com/hamster-wheel-derby',
     audience: 'Hamster Owners and Enthusiasts',
     creator_id: 8,
   },
@@ -113,8 +113,8 @@ export const addEvents = [
     location: 'Fitness Park, 345 Healthy Way, Pet City',
     description:
       '💪🐾 Join us at the Pet Fitness Expo to discover the best ways to keep your pets active and healthy! Featuring fitness demos, pet exercise classes, and expert talks on pet wellness.',
-    eventImage: 'https://example.com/images/pet_fitness_expo.jpg',
-    eventWebsite: 'https://www.example.com/pet-fitness-expo',
+    event_image: 'https://example.com/images/pet_fitness_expo.jpg',
+    event_website: 'https://www.example.com/pet-fitness-expo',
     audience: 'Pet Owners and Health Enthusiasts',
     creator_id: 9,
   },
@@ -126,8 +126,8 @@ export const addEvents = [
     location: "Fluffy's Bistro, 678 Carrot Lane, Bunnyville",
     description:
       '🥕🐇 Enjoy a delightful brunch while bonding with your bunnies and other rabbit lovers! The event features bunny-friendly foods, educational talks on rabbit care, and a bunny play area.',
-    eventImage: 'https://example.com/images/bunny_brunch.jpg',
-    eventWebsite: 'https://www.example.com/bunny-bonding-brunch',
+    event_image: 'https://example.com/images/bunny_brunch.jpg',
+    event_website: 'https://www.example.com/bunny-bonding-brunch',
     audience: 'Rabbit Owners',
     creator_id: 10,
   },
@@ -139,8 +139,8 @@ export const addEvents = [
     location: 'Creative Studio, 890 Art Avenue, Pet City',
     description:
       '🎨🐾 Unleash your creativity at our Pets & Paints Art Workshop! Create beautiful pet portraits or abstract art with guidance from local artists. All materials provided, and pets are welcome!',
-    eventImage: 'https://example.com/images/pets_paints_workshop.jpg',
-    eventWebsite: 'https://www.example.com/pets-paints-workshop',
+    event_image: 'https://example.com/images/pets_paints_workshop.jpg',
+    event_website: 'https://www.example.com/pets-paints-workshop',
     audience: 'Art Lovers and Pet Owners',
     creator_id: 11,
   },
@@ -152,8 +152,8 @@ export const addEvents = [
     location: 'Snowy Park, 234 Frosty Lane, Winterville',
     description:
       '❄️🐕 Celebrate the winter season with your pets at our Pet Winter Wonderland! Enjoy snow-themed games, hot cocoa for humans, and festive pet treats in a winter wonderland setting.',
-    eventImage: 'https://example.com/images/pet_winter_wonderland.jpg',
-    eventWebsite: 'https://www.example.com/pet-winter-wonderland',
+    event_image: 'https://example.com/images/pet_winter_wonderland.jpg',
+    event_website: 'https://www.example.com/pet-winter-wonderland',
     audience: 'Pet Owners',
     creator_id: 12,
   },
