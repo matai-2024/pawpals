c11cb9daff68fd03bed3193e7fa088861537606c
achoo-o <amyj.etana@gmail.com>
2024-09-15 17:24:48 +1200

    add profile banner style excl. owner profile

--- client/pages/Profile.tsx
+++ client/pages/Profile.tsx
@@ -29,138 +29,115 @@ export default function Profile() {
 
   return (
     <>
-      <div className="mx-auto max-w-5xl py-32 sm:py-48 lg:py-32">
-        <div id="profile" className="flex flex-col">
-          <div>
-            <div className="mb-6 w-52 h-52 rounded-full overflow-hidden">
-              <img
-                className="relative w-full -top-12"
-                src={data.image ? `../../${data.image}` : `../../miso.jpg`}
-                alt={data.petName}
-              />
+    <section className="flex-column align-middle my-20 w-full">
+      <div className="flex flex-row gap-16">
+      <img
+      className="w-64 max-w-64 rounded-3xl"
+      src={data.image ? `../../${data.image}` : `../../miso.jpg`}
+      alt={data.petName}
+      />
+      <div className="flex-column">
+        <h1 className="my-11 text-6xl font-bold text-black">
+          {data.petName ? data.petName : 'Cutie Patootie'}
+        </h1>
+        <p className="text-2xl leading-8 text-gray-950">
+          {getAge(data.dateOfBirth) > 1 || getAge(data.dateOfBirth) == 0
+          ? `${getAge(data.dateOfBirth)}yrs old`
+          : `${getAge(data.dateOfBirth)}yr old`}
+          {data.gender
+          ? `, ${data.gender}`
+          : ``}
+          {data.breed
+          ? `, ${data.breed}`
+          : ``}
+        </p>
+          <div className="text-md text-gray-950 font-medium mt-6 flex flex-wrap items-center justify-start gap-2 lg:gap-3">
+              {data.busy === 'true' ? (
+                <span className="bg-yellow-400 rounded-full py-1 px-3">Busy</span>
+              ) : (
+                ''
+              )}
+              {data.lazy === 'true' ? (
+                <span className="bg-yellow-400 rounded-full py-1 px-3">Lazy</span>
+              ) : (
+                ''
+              )}
+              {data.goofy === 'true' ? (
+                <span className="bg-yellow-400 rounded-full py-1 px-3">
+                  Goofy
+                </span>
+              ) : (
+                ''
+              )}
+              {data.gorgeous === 'true' ? (
+                <span className="bg-yellow-400 rounded-full py-1 px-3">
+                  Gorgeous
+                </span>
+              ) : (
+                ''
+              )}
+              {data.brat === 'true' ? (
+                <span className="bg-yellow-400 rounded-full py-1 px-3">Brat</span>
+              ) : (
+                ''
+              )}
+              {data.loyal === 'true' ? (
+                <span className="bg-yellow-400 rounded-full py-1 px-3">
+                  Loyal
+                </span>
+              ) : (
+                ''
+              )}
+              {data.playful === 'true' ? (
+                <span className="bg-yellow-400 rounded-full py-1 px-3">
+                  Playful
+                </span>
+              ) : (
+                ''
+              )}
+              {data.adventurous === 'true' ? (
+                <span className="bg-yellow-400 rounded-full py-1 px-3">
+                  Adventurous
+                </span>
+              ) : (
+                ''
+              )}
+              {data.foodie === 'true' ? (
+                <span className="bg-yellow-400 rounded-full py-1 px-3">
+                  Foodie
+                </span>
+              ) : (
+                ''
+              )}
+              {data.snorer === 'true' ? (
+                <span className="bg-yellow-400 rounded-full py-1 px-3">
+                  Snorer
+                </span>
+              ) : (
+                ''
+              )}
+              {data.crazy === 'true' ? (
+                <span className="bg-yellow-400 rounded-full py-1 px-3">
+                  Crazy
+                </span>
+              ) : (
+                ''
+              )}
+              {data.floofy === 'true' ? (
+                <span className="bg-yellow-400 rounded-full py-1 px-3">
+                  Floofy
+                </span>
+              ) : (
+                ''
+              )}
             </div>
-            <div>
-              <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-3xl">
-                {data.petName ? data.petName : 'Cutie Patootie'}
-              </h1>
-              <div className="flex gap-x-4">
-                <p className="text-lg leading-8 text-gray-600">
-                  {getAge(data.dateOfBirth) > 1
-                    ? `${getAge(data.dateOfBirth)}yrs old`
-                    : `${getAge(data.dateOfBirth)}yr old`}
-                </p>
-                <p className="text-lg leading-8 text-gray-600">{data.gender}</p>
-                <p className="text-lg leading-8 text-gray-600">{data.breed}</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="mt-6 flex items-center justify-center gap-x-6">
-            <p className="text-xl font-semibold leading-8 text-gray-600">
-              {data.dateOfBirth.split('-').reverse().join('-')}
-            </p>
-            <p className="text-xl font-semibold leading-8 text-gray-600">
-              {data.gender}
-            </p>
-            <p className="text-xl font-semibold leading-8 text-gray-600">
-              {data.breed}
-            </p>
-          </div>
-          <div className="mt-6 flex flex-wrap items-center justify-center gap-2 lg:gap-4">
-            {data.busy === 'true' ? (
-              <span className="bg-yellow-400 rounded-full py-1 px-4">Busy</span>
-            ) : (
-              ''
-            )}
-            {data.lazy === 'true' ? (
-              <span className="bg-yellow-400 rounded-full py-1 px-4">Lazy</span>
-            ) : (
-              ''
-            )}
-            {data.goofy === 'true' ? (
-              <span className="bg-yellow-400 rounded-full py-1 px-4">
-                Goofy
-              </span>
-            ) : (
-              ''
-            )}
-            {data.gorgeous === 'true' ? (
-              <span className="bg-yellow-400 rounded-full py-1 px-4">
-                Gorgeous
-              </span>
-            ) : (
-              ''
-            )}
-            {data.brat === 'true' ? (
-              <span className="bg-yellow-400 rounded-full py-1 px-4">Brat</span>
-            ) : (
-              ''
-            )}
-            {data.loyal === 'true' ? (
-              <span className="bg-yellow-400 rounded-full py-1 px-4">
-                Loyal
-              </span>
-            ) : (
-              ''
-            )}
-            {data.playful === 'true' ? (
-              <span className="bg-yellow-400 rounded-full py-1 px-4">
-                Playful
-              </span>
-            ) : (
-              ''
-            )}
-            {data.adventurous === 'true' ? (
-              <span className="bg-yellow-400 rounded-full py-1 px-4">
-                Adventurous
-              </span>
-            ) : (
-              ''
-            )}
-            {data.foodie === 'true' ? (
-              <span className="bg-yellow-400 rounded-full py-1 px-4">
-                Foodie
-              </span>
-            ) : (
-              ''
-            )}
-            {data.snorer === 'true' ? (
-              <span className="bg-yellow-400 rounded-full py-1 px-4">
-                Snorer
-              </span>
-            ) : (
-              ''
-            )}
-            {data.crazy === 'true' ? (
-              <span className="bg-yellow-400 rounded-full py-1 px-4">
-                Crazy
-              </span>
-            ) : (
-              ''
-            )}
-            {data.floofy === 'true' ? (
-              <span className="bg-yellow-400 rounded-full py-1 px-4">
-                Floofy
-              </span>
-            ) : (
-              ''
-            )}
-          </div>
-          <p className="mt-6 text-lg leading-8 text-gray-600">{data.bio}</p>
         </div>
       </div>
-      <div
-        aria-hidden="true"
-        className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-      >
-        <div
-          style={{
-            clipPath:
-              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-          }}
-          className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-        />
-      </div>
+        <h2 className="mt-16 mx-auto mb-5 text-xl font-bold w-11/12">About me</h2>
+        <p className="mx-auto w-11/12">{data.bio
+          ? data.bio
+          : `I don't have a description yet, but don't I look cute?`}</p>
+    </section>
     </>
   )
 }
