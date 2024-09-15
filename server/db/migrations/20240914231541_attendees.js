205fa5d7b9d72d8fb0703037ee7f8a44bca4c1a7
Anahera Foley-Paama <a.foleypaama@gmail.com>
2024-09-15 16:25:32 +1200

    Update attendees db to include pet_id column and update seed data

--- server/db/migrations/20240914231541_attendees.js
+++ server/db/migrations/20240914231541_attendees.js
@@ -1,8 +1,9 @@
 export function up(knex) {
   return knex.schema.createTable('attendees', (table) => {
     table.increments('id').primary()
-    table.integer('account_id').references('owners.id')
     table.integer('event_id').references('events.id')
+    table.integer('account_id').references('owners.id')
+    table.integer('pet_id').references('pets.id')
   })
 }
 
