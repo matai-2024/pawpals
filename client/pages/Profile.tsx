5b028202991817f8e0011d2775898e15458409cb
Anahera Foley-Paama <a.foleypaama@gmail.com>
2024-09-15 19:48:00 +1200

    Styling and lint test: Reset aria hidden label to origin boolean for the bottom gradient

--- client/pages/Profile.tsx
+++ client/pages/Profile.tsx
@@ -158,7 +158,7 @@ export default function Profile() {
         </div>
 
         <div
-          aria-hidden="on"
+          aria-hidden="true"
           className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
         >
           <div
