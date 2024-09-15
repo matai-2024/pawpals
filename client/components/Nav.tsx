9a8b8fcdd35a5b6888f18672bf04506f60919498
Anahera Foley-Paama <a.foleypaama@gmail.com>
2024-09-15 17:01:00 +1200

    Update logo

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -32,8 +32,8 @@ export default function Nav() {
       >
         <div className="flex lg:flex-1">
           <Link to="/" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
-            <img alt="logo" src="/logo.png" className="h-14 w-auto" />
+            <span className="sr-only">pawpals</span>
+            <img alt="logo" src="/pawpals.png" className="h-16 w-auto" />
           </Link>
         </div>
         <div className="flex lg:hidden">
