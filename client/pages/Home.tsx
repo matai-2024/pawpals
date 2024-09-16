8a4d79be43ff9c23e45992481d830bcefae58546
Jack <jackgloyer8@gmail.com>
2024-09-17 11:37:10 +1200

    redirect to about page

--- client/pages/Home.tsx
+++ client/pages/Home.tsx
@@ -24,13 +24,13 @@ export default function Home() {
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <Link
-              to="/create"
+              to={`${window.location.origin}/user-profile`}
               className="rounded-md bg-yellow-400 px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Get started
             </Link>
             <Link
-              to="#"
+              to="/about"
               className="text-sm font-semibold leading-6 text-gray-900"
             >
               Learn more <span aria-hidden="true">→</span>
