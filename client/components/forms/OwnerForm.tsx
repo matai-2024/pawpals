772443b81fc518b38d7f99d31a7d5bb3d4dbad54
Amy <58102593+achoo-o@users.noreply.github.com>
2024-09-16 16:02:56 +1200

    style form

--- client/components/forms/OwnerForm.tsx
+++ client/components/forms/OwnerForm.tsx
@@ -27,69 +27,53 @@ export default function OwnerForm({
   }
 
   return (
-    <FormWrapper title="Create an account">
-      <form onSubmit={handleSubmit}>
-        <div className="sm:col-span-3">
-          <label
-            className="block text-sm font-medium leading-6 text-gray-900"
-            htmlFor="firstName"
-          >
-            First name
-          </label>
-          <div className="mt-2">
-            <input
-              id="firstName"
-              name="firstName"
-              type="text"
-              placeholder="First name"
-              onChange={handleChange}
-              value={form.firstName}
-              className="form-input block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
+    <div className="flex-col my-32">
+      <FormWrapper title="Create an account">
+        <form onSubmit={handleSubmit}>
+          <div className="sm:col-span-3 my-5">
+            <label
+              className="block text-sm font-medium leading-6 text-gray-900"
+              htmlFor="firstName"
+            >
+              First name
+            </label>
+            <div className="mt-2">
+              <input
+                id="firstName"
+                name="firstName"
+                type="text"
+                placeholder="First name"
+                onChange={handleChange}
+                value={form.firstName}
+                className="form-input block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
           </div>
-        </div>
 
-        <div className="sm:col-span-3">
-          <label
-            className="block text-sm font-medium leading-6 text-gray-900"
-            htmlFor="lastName"
-          >
-            Last name
-          </label>
-          <div className="mt-2">
-            <input
-              id="lastName"
-              name="lastName"
-              type="text"
-              placeholder="Last name"
-              onChange={handleChange}
-              value={form.lastName}
-              className="form-input block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
+          <div className="sm:col-span-3">
+            <label
+              className="block text-sm font-medium leading-6 text-gray-900"
+              htmlFor="lastName"
+            >
+              Last name
+            </label>
+            <div className="mt-2">
+              <input
+                id="lastName"
+                name="lastName"
+                type="text"
+                placeholder="Last name"
+                onChange={handleChange}
+                value={form.lastName}
+                className="form-input block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
           </div>
-        </div>
-
-        <div className="sm:col-span-3">
-          <label
-            className="block text-sm font-medium leading-6 text-gray-900"
-            htmlFor="lastName"
-          >
-            Email
-          </label>
-          <div className="mt-2">
-            <input
-              id="email"
-              name="email"
-              type="text"
-              placeholder="Email"
-              onChange={handleChange}
-              value={form.email}
-              className="form-input block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-          </div>
-        </div>
-        <button>Submit</button>
-      </form>
-    </FormWrapper>
+          <button className="my-7 rounded-md bg-yellow-400 px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            Submit
+          </button>
+        </form>
+      </FormWrapper>
+    </div>
   )
 }
