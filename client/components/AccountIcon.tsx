0d83855379319d702094329941c4ca97669ffa29
Amy Jackson <amyj.etana@gmail.com>
2024-09-17 11:32:38 +1200

    refactor dropdown-content, add style

--- client/components/AccountIcon.tsx
+++ client/components/AccountIcon.tsx
@@ -7,6 +7,8 @@ export default function AccountIcon() {
     logout()
   }
 
+  const navigation = [{ name: 'Log out', handleClick: handleSignOut }]
+
   function handleClick() {
     const dropdowns = document.querySelector('.dropdown-content')
     if (dropdowns?.classList.contains('hidden')) {
@@ -17,17 +19,29 @@ export default function AccountIcon() {
   }
 
   return (
-    <div>
-      <img
-        onClick={handleClick}
-        src={`${user?.picture}`}
-        className="relative rounded-full max-w-11"
-      ></img>
-      <div className="absolute right-5 dropdown-content hidden mt-2 p-2 text-gray-600 border-2">
-        <a className="" onClick={handleSignOut}>
-          Log out
-        </a>
-      </div>
+    // <div>
+    //   <img
+    //     onClick={handleClick}
+    //     src={`${user?.picture}`}
+    //     className="relative rounded-full max-w-11"
+    //   ></img>
+    //   <div className="absolute right-5 dropdown-content hidden mt-2 p-2 text-gray-600 border-2">
+    //     <a className="" onClick={handleSignOut}>
+    //       Log out
+    //     </a>
+    //   </div>
+    // </div>
+    <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in">
+      {navigation.map((item) => (
+        <div key={item.name}>
+          <a
+            onClick={item.handleClick}
+            className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
+          >
+            {item.name}
+          </a>
+        </div>
+      ))}
     </div>
   )
 }
