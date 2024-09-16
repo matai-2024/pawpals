c17abe6f14f99630cd6cb1e69541c482f260dccd
Amy <58102593+achoo-o@users.noreply.github.com>
2024-09-16 15:54:07 +1200

    clean up hook

--- client/hooks/ownerHooks/use-create-owner-profile.ts
+++ client/hooks/ownerHooks/use-create-owner-profile.ts
@@ -1,24 +1,10 @@
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
-import { useAuth0 } from '@auth0/auth0-react'
+import { useMutation } from '@tanstack/react-query'
 import { useNavigate } from 'react-router-dom'
-import { createOwner, getOwnerInfo } from '../../apis/apiClientOwners'
+import { createOwner } from '../../apis/apiClientOwners'
 import { CreateOwnerData } from '../../../models/forms'
 
 function useOwnerProfile() {
   const navigate = useNavigate()
-  // const { user, getAccessTokenSilently } = useAuth0()
-
-  // const queryClient = useQueryClient()
-  // const { data, isLoading } = useQuery({
-  //   queryKey: ['owner'],
-  //   queryFn: async () => {
-  //     const accessToken = await getAccessTokenSilently()
-  //     if (user && user.sub) {
-  //       const response = await getOwnerInfo(accessToken)
-  //       return response
-  //     }
-  //   },
-  // })
 
   const mutation = useMutation({
     mutationFn: ({ form, token }: { form: CreateOwnerData; token: string }) =>
