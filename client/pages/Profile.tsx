891a16b53fc482fccd3227ed0b64b2924018fe63
Dean <171200457+dean-tunbridge@users.noreply.github.com>
2024-09-16 14:23:08 +1200

    change miso.jpg to miso.webp

--- client/pages/Profile.tsx
+++ client/pages/Profile.tsx
@@ -52,7 +52,7 @@ export default function Profile() {
               <div className="w-48 h-48 overflow-hidden rounded-full border border-4 border-white">
                 <img
                   className="object-cover min-h-48 relative -top-8"
-                  src={data.image ? `../../${data.image}` : `../../miso.jpg`}
+                  src={data.image ? `../../${data.image}` : `../../miso.webp`}
                   alt={data.petName}
                 />
               </div>
