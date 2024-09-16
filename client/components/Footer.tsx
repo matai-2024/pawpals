714cfec6935506a49852113307f1a578880601a6
Dean <171200457+dean-tunbridge@users.noreply.github.com>
2024-09-16 16:22:59 +1200

    update Link tags with to=''

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 export default function Footer() {
   return (
     <>
@@ -6,47 +8,47 @@ export default function Footer() {
           <nav className="flex flex-wrap justify-center -mx-5 -my-2">
             <div className="px-5 py-2">
               <Link
-                href="#"
-                className="text-base leading-6 text-gray-950 hover:text-gray-900"
+                to="/about"
+                className="text-base leading-6 text-gray-950 hover:text-yellow-400"
               >
                 About
               </Link>
             </div>
             <div className="px-5 py-2">
               <Link
-                href="#"
-                className="text-base leading-6 text-gray-950 hover:text-gray-900"
+                to="/profiles"
+                className="text-base leading-6 text-gray-950 hover:text-yellow-400"
               >
                 Pets
               </Link>
             </div>
             <div className="px-5 py-2">
               <Link
-                href="#"
-                className="text-base leading-6 text-gray-950 hover:text-gray-900"
+                to="#"
+                className="text-base leading-6 text-gray-950 hover:text-yellow-400"
               >
                 Events
               </Link>
             </div>
             <div className="px-5 py-2">
               <Link
-                href="#"
-                className="text-base leading-6 text-gray-950 hover:text-gray-900"
+                to="#"
+                className="text-base leading-6 text-gray-950 hover:text-yellow-400"
               >
                 Log in
               </Link>
             </div>
             <div className="px-5 py-2">
               <Link
-                href="#"
-                className="text-base leading-6 text-gray-950 hover:text-gray-900"
+                to="#"
+                className="text-base leading-6 text-gray-950 hover:text-yellow-400"
               >
                 Sign up
               </Link>
             </div>
           </nav>
           <div className="flex justify-center mt-8 space-x-6">
-            <Link href="#" className="text-gray-400 hover:text-gray-950">
+            <Link to="#" className="text-gray-400 hover:text-gray-950">
               <span className="sr-only">Facebook</span>
               <svg
                 className="w-6 h-6"
@@ -61,7 +63,7 @@ export default function Footer() {
                 ></path>
               </svg>
             </Link>
-            <Link href="#" className="text-gray-400 hover:text-gray-950">
+            <Link to="#" className="text-gray-400 hover:text-gray-950">
               <span className="sr-only">Instagram</span>
               <svg
                 className="w-6 h-6"
@@ -75,8 +77,8 @@ export default function Footer() {
                   clipRule="evenodd"
                 ></path>
               </svg>
-            </a>
-            <a href="#" className="text-gray-400 hover:text-gray-950">
+            </Link>
+            <Link to="#" className="text-gray-400 hover:text-gray-950">
               <span className="sr-only">Twitter</span>
               <svg
                 className="w-6 h-6"
@@ -86,8 +88,8 @@ export default function Footer() {
               >
                 <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"></path>
               </svg>
-            </a>
-            <a href="#" className="text-gray-400 hover:text-gray-950">
+            </Link>
+            <Link to="#" className="text-gray-400 hover:text-gray-950">
               <span className="sr-only">GitHub</span>
               <svg
                 className="w-6 h-6"
@@ -101,8 +103,8 @@ export default function Footer() {
                   clipRule="evenodd"
                 ></path>
               </svg>
-            </a>
-            <a href="#" className="text-gray-400 hover:text-gray-950">
+            </Link>
+            <Link to="#" className="text-gray-400 hover:text-gray-950">
               <span className="sr-only">Dribbble</span>
               <svg
                 className="w-6 h-6"
@@ -116,10 +118,10 @@ export default function Footer() {
                   clipRule="evenodd"
                 ></path>
               </svg>
-            </a>
+            </Link>
           </div>
           <p className="mt-8 text-base leading-6 text-center text-gray-400">
-            © 2021 SomeCompany, Inc. All rights reserved.
+            © 2024 pawpals, Inc. All rights reserved.
           </p>
         </div>
       </section>
