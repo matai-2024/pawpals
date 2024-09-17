5f3bded8340a5630471aa0b07a4c7cd506c1a7b3
Amy Jackson <amyj.etana@gmail.com>
2024-09-17 16:11:20 +1200

    remove unused var

--- client/components/UserMenu.tsx
+++ client/components/UserMenu.tsx
@@ -30,7 +30,7 @@ export default function AccountIcon({ handleClick }: Props) {
       </button>
 
       <div className="dropdown-content hidden absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in">
-        {navigation.map((item, i) => (
+        {navigation.map((item) => (
           <a
             tabIndex={0}
             key={item.name}
