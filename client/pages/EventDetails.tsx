656e7e8328a4079f6eba9f5cf0dc246cf2a3a799
Anahera Foley-Paama <a.foleypaama@gmail.com>
2024-09-18 11:58:37 +1200

    npm run typecheck

--- client/pages/EventDetails.tsx
+++ client/pages/EventDetails.tsx
@@ -107,9 +107,7 @@ export default function EventDetails() {
               <p>{event.description}</p>
             </div>
             <div>
-              <button
-                className="w-full rounded-md bg-yellow-400 hover:bg-yellow-500 ease-in-out duration-200 px-3.5 py-2.5 text-lg font-semibold text-gray-900 shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
+              <button className="w-full rounded-md bg-yellow-400 hover:bg-yellow-500 ease-in-out duration-200 px-3.5 py-2.5 text-lg font-semibold text-gray-900 shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                 Attend
               </button>
             </div>
