2b95c65abffbb3f407cd83a638835cb3678f580e
Sam Pedersen <sjcfpedersen@gmail.com>
2024-09-18 11:57:14 +1200

    not ham image fallback

--- client/components/utils/ScheduleCard/ScheduleCard.tsx
+++ client/components/utils/ScheduleCard/ScheduleCard.tsx
@@ -27,7 +27,7 @@ export default function ScheduleCard({
   editBtn,
   cancelBtn,
 }: ScheduleCardProps) {
-  const fallbackimage = '/hiddenmeme.jpg'
+  const fallbackimage = '/events/event-9.webp'
   return (
     <div className="flex items-center border w-full p-3 rounded-lg gap-6 shadow-md ease-in-out duration-200">
       <div className="w-36 h-24 rounded-md overflow-hidden">
