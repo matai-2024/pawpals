8ea3f3edd95430c0a10088085f02701a8ef5d985
Anahera Foley-Paama <a.foleypaama@gmail.com>
2024-09-18 17:16:57 +1200

    Start attend event component

--- client/pages/EventDetails.tsx
+++ client/pages/EventDetails.tsx
@@ -4,6 +4,7 @@ import LoadingSpinner from '../components/LoadingSpinner'
 import useEventById from '../hooks/eventHooks/use-event-by-id.ts'
 import { useAuth0 } from '@auth0/auth0-react'
 import { useAttendeeByEventId, useOwnerByEventId } from '../hooks/hooks.ts'
+import AttendEvent from '../components/forms/attend-event/AttendEvent.tsx'
 
 export default function EventDetails() {
   const { user, isAuthenticated } = useAuth0()
@@ -107,9 +108,7 @@ export default function EventDetails() {
               <p>{event.description}</p>
             </div>
             <div>
-              <button className="w-full rounded-md bg-yellow-400 hover:bg-yellow-500 ease-in-out duration-200 px-3.5 py-2.5 text-lg font-semibold text-gray-900 shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                Attend
-              </button>
+              <AttendEvent />
             </div>
           </div>
         </div>
