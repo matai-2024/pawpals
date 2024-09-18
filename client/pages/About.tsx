aeb57ecd05b447e148ea772cd4f0ef00965564cd
Anahera Foley-Paama <a.foleypaama@gmail.com>
2024-09-18 15:26:46 +1200

    Complete About page styling

--- client/pages/About.tsx
+++ client/pages/About.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 export default function About() {
   const teamMembers = [
     {
@@ -48,92 +50,121 @@ export default function About() {
         <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
           About
         </h1>
-        <p className="block my-6 text-lg leading-8 text-gray-600">
-          We are the pawpals team!
-        </p>
       </div>
 
-      <div className="flex flex-row items-center">
+      <div className="flex flex-row items-center gap-6 py-10">
         <div className="w-1/2 p-6">
-          <h2 className="text-4xl font-semibold text-gray-800">
-            The Animals that made this
+          <h2 className="text-4xl font-semibold text-gray-900">
+            Letting pets live their best lives
           </h2>
-          <p className="text-xl font-semibold text-gray-600 mt-2">
-            Who is Who in the ZOO!!
+          <p className="text-lg text-gray-700 mb-4 mt-8">
+            Pawpals is a community-driven website designed to connect pet lovers
+            and pet-friendy events, whether you have a pet or not.
           </p>
-          <p className="mt-4 text-base text-gray-700">
-            Pawpals is a community-driven website designed to connect pet
-            lovers, whether they have pets or not. Users can browse adorable
-            pets, explore pet-friendly events, or even create their own. The
-            platform allows users to create personalized profiles for their pets
-            and join events hosted by other members.
+          <p className="text-lg text-gray-700 mb-10">
+            Browse adorable pets, learn their personalities, explore your local
+            pet-friendly events, or even create your own!
           </p>
-          <p className="mt-4 text-base text-gray-700">
+          <Link
+            to="/create"
+            className="rounded-md bg-yellow-400 hover:bg-yellow-500 px-8 py-4 text-md font-semibold text-gray-900 shadow-sm ease-in-out duration-200"
+          >
+            Get started
+            <span aria-hidden="true" className="pl-3">
+              →
+            </span>
+          </Link>
+        </div>
+        <div className="w-1/2 p-6">
+          <img
+            className="mt-4 w-full rounded-lg object-cover"
+            src="/pets/pet-event.webp"
+            alt="A cutie dog at a outdoor event"
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-row-reverse items-center gap-6 py-10">
+        <div className="w-1/2 p-6">
+          <h2 className="text-4xl font-semibold text-gray-900">
+            The animals that made this
+          </h2>
+          <p className="text-lg text-gray-700 mb-4 mt-8">
             This project was conceptualized by Anahera and developed in
             collaboration with Jack, Dean, Sam, and Amy as part of the pawpals
             team!
           </p>
+          <p className="text-lg text-gray-700 mb-4">
+            pawpals is our final group project for{' '}
+            <Link
+              target="_blank"
+              to={'https://devacademy.co.nz/course-details/'}
+              className="underline text-blue-950 hover:text-blue-800"
+            >
+              Dev Academy&apos;s Full Stack Web Development course
+            </Link>
+            , graduating October 2024.
+          </p>
+          <p className="text-lg text-gray-700">Find out more about us below.</p>
         </div>
         <div className="w-1/2 p-6">
           <img
             className="mt-4 w-full rounded-lg object-cover"
             src="team.webp"
-            alt="coders at work"
+            alt="pawpal coders at work"
           />
         </div>
       </div>
+
       <div>
-        {/* Team Section */}
-        <div className="bg-white p-8 rounded-lg shadow">
-          <h2 className="text-center text-2xl font-semibold text-gray-800">
+        <div className="py-10">
+          <h2 className="text-center text-4xl font-semibold text-gray-900">
             Our Team
           </h2>
-          <p className="text-center text-xl text-gray-600 mt-2">
-            The crazy cats who made this
-          </p>
-
-          <div className="flex gap-6 mt-10 overflow-x-auto">
+          <div className="flex lg:flex-row flex-col gap-6 py-10 overflow-x-auto">
             {teamMembers.map((member) => (
               <div
                 key={member.name}
-                className="flex flex-col items-center bg-white p-6 rounded-lg shadow hover:bg-gray-100 min-w-[150px]"
+                className="flex flex-col items-center w-1/5 p-6 bg-white border rounded-lg shadow-lg hover:shadow-xl ease-in-out duration-200"
               >
-                <img
-                  className="w-24 h-24 sm:w-32 sm:h-32 rounded-full object-cover mb-4"
-                  src={member.img}
-                  alt={`${member.name}'s profile`}
-                />
+                <div className="w-24 h-24 rounded-full overflow-hidden shadow-lg mb-6">
+                  <img
+                    className="object-cover relative"
+                    src={member.img}
+                    alt={`${member.name}'s profile`}
+                  />
+                </div>
 
                 <div className="text-center">
                   <h3 className="text-xl font-semibold text-gray-800">
                     {member.name}
                   </h3>
-                  <p className="text-gray-600">{member.role}</p>
+                  <p className="text-gray-600 mb-6">{member.role}</p>
 
                   <div className="flex justify-center space-x-4 mt-4">
-                    <a
-                      href={member.linkedin}
+                    <Link
+                      to={member.linkedin}
                       target="_blank"
                       rel="noopener noreferrer"
                       aria-label={`${member.name}'s LinkedIn`}
                     >
-                      <i className="text-2xl fa-brands fa-linkedin"></i>
-                    </a>
+                      <i className="text-2xl fa-brands fa-linkedin text-gray-700 hover:text-blue-900"></i>
+                    </Link>
 
-                    <a
-                      href={`mailto:${member.email}`}
+                    <Link
+                      to={`mailto:${member.email}`}
                       aria-label={`${member.name}'s email`}
                     >
-                      <i className="text-2xl fa-solid fa-envelope"></i>
-                    </a>
-                    <a
-                      href={member.github}
+                      <i className="text-2xl fa-solid fa-envelope text-gray-700 hover:text-blue-900"></i>
+                    </Link>
+                    <Link
+                      to={member.github}
                       target="_blank"
                       rel="noopener noreferrer"
                       aria-label={`${member.name}'s github`}
                     >
-                      <i className="text-2xl fa-brands fa-square-github"></i>
-                    </a>
+                      <i className="text-2xl fa-brands fa-square-github text-gray-700 hover:text-blue-900"></i>
+                    </Link>
                   </div>
                 </div>
               </div>
